6f977966f1af4004a10858acb4a003c5180b0cab
Anthony P. Tran <atranp@gmail.com>
2022-04-26 16:05:16 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,33 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var generateRandom = document.getElementById("generateRandom");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")" ;
+
+	css.textContent = body.style.background + ";" ;
+}
+
+function randomColor() {
+	return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function setBg() {
+	color1.value = "#" + randomColor();
+	color2.value = "#" + randomColor();	
+	setGradient();
+
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+generateRandom.addEventListener("click", setBg);
